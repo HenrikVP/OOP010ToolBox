36cc321a92f9e2c7adc44fbfe39a8aa0616f4426
Henrik Vincents Poulsen <hpo@tec.dk>
2022-09-06 10:34:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP010ToolBox;
+
+ToolBox toolBox = new();
+
+toolBox.Tools = new List<Tool>();
+
+Tool t1 = new Tool();
+t1.Name = "Hammer";
+t1.Type = ToolType.HandTools;
+
+Tool t2 = new Tool("Saw", ToolType.HandTools);
+
+Tool t3 = new Tool() { Name = "Screwdriver", Type = ToolType.HandTools };
+
+toolBox.Tools.Add(t1);
+toolBox.Tools.Add(t2);
+toolBox.Tools.Add(t3);
+
+foreach (Tool t in toolBox.Tools)
+{
+    Console.WriteLine($"Tool: {t.Name} Type: {t.Type}");
+}
---
+++ ToolBox.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP010ToolBox
+{
+    internal class ToolBox
+    {
+        public List<Tool> Tools { get; set; }
+    }
+
+    public enum ToolType { HandTools, PowerTools , Fasteners}
+
+    public class Tool
+    {
+        private string name;
+        private ToolType toolType;
+
+        public Tool(){}
+        public Tool(string name, ToolType type)
+        {
+            this.name = name;
+            toolType = type;
+        }
+        public string Name {
+            get { return name; }
+            set { name = value; } 
+        }
+        public ToolType Type {
+            get { return toolType; }
+            set { toolType = value; }
+        }
+    }
+}
